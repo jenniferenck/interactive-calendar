eca87e297d0e03caa6c1401a75bbefd369cc9778
Jennifer Enck <jenniferenck@mac.com>
2019-07-30 12:58:36 -0700

    added struture to begin adding diff cal views

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import CalendarMonth from './CalendarMonth';
 
-function App() {
-  return (
-    <div className="App">
-      <CalendarMonth />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { calendarView: 'month' }; // options to view month, week or day
+  }
+
+  renderCurrentCalendarView = () => {
+    if (this.state.calendarView === 'month') {
+      return (
+        <>
+          <CalendarMonth />
+          <CalendarMonth />
+        </>
+      );
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {/* display current month first followed by suceeding month */}
+        {this.renderCurrentCalendarView()}
+
+        {/* <CalendarMonth /> */}
+      </div>
+    );
+  }
 }
 
 export default App;

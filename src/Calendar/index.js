09e15c68506cbffbf89da856d8688d973391adf6
Jennifer Enck <jenniferenck@mac.com>
2019-08-07 09:29:56 -0700

    added function to shift months on click

--- src/Calendar/index.js
+++ src/Calendar/index.js
@@ -24,13 +24,44 @@ class Calendar extends Component {
     return firstMonthIndex < 11 ? firstYear : firstYear + 1;
   };
 
+  shiftMonths = direction => {
+    const { firstMonthIndex } = this.state;
+    if (direction === 'previous') {
+      if (firstMonthIndex > 0) {
+        this.setState(st => ({ firstMonthIndex: st.firstMonthIndex - 1 }));
+      } else {
+        this.setState(st => ({
+          firstMonthIndex: 11,
+          firstYear: st.firstYear - 1
+        }));
+      }
+    } else {
+      if (firstMonthIndex < 11) {
+        this.setState(st => ({ firstMonthIndex: st.firstMonthIndex + 1 }));
+      } else {
+        this.setState(st => ({
+          firstMonthIndex: 0,
+          firstYear: st.firstYear + 1
+        }));
+      }
+    }
+  };
+
   render() {
+    console.log(this.state);
     const { firstMonthIndex, firstYear } = this.state;
     return (
       <div className="calendar">
         <div className="arrows">
-          <div className="left-arrow">Previous Month</div>
-          <div className="right-arrow">Next Month</div>
+          <div
+            onClick={() => this.shiftMonths('previous')}
+            className="left-arrow"
+          >
+            Previous Month
+          </div>
+          <div onClick={() => this.shiftMonths('next')} className="right-arrow">
+            Next Month
+          </div>
         </div>
 
         <div className="months">
